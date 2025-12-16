dc8846764cb013ac9555c9da87f9cc310093da50
krzpag <Krzysztof-Tomasz.Pagacz@t-systems.com>
2025-12-16 11:35:43 +0100

    add apps folder, update variabless

--- stack/mariadb-operator/main.tf
+++ stack/mariadb-operator/main.tf
@@ -1,33 +1,21 @@
 locals {
-  mariadb_config = yamldecode(file("${path.module}/../../configuration/mariadb-operator.yaml"))
+  mariadb_config = yamldecode(file("${path.module}/../../configuration/mariadb.yaml"))
 }
 
 resource "helm_release" "mariadb_operator" {
-  name       = local.mariadb_config["release_name"]
-  chart      = "${path.module}/../../charts/mariadb-operator"
-  namespace  = local.mariadb_config["namespace"]
-  create_namespace = true
-}
+  for_each          = local.mariadb_config
 
-resource "kubernetes_manifest" "mariadb_instance" {
-  manifest = {
-    apiVersion = "mariadb.mmontes.io/v1alpha1"
-    kind       = "MariaDB"
-    metadata = {
-      name      = "mariadb-instance"
-      namespace = "default"
-    }
-    spec = {
-      rootPasswordSecretKeyRef = {
-        name = kubernetes_secret.mariadb_secret.metadata[0].name
-        key  = "password"
-      }
-      image    = "mariadb:11.4"
-      replicas = 1
-      storage = {
-        size = "1Gi"
-      }
-    }
-  }
-}
+  chart             = "${path.module}/../../apps/mariadb-operator"
+  description       = "Deploys MariaDB helm chart within the cluster as an ArgoCD Application"
 
+  name              = each.key
+  namespace         = each.value.namespace
+  dependency_update = true
+  create_namespace  = true
+
+  values = [
+    yamlencode({
+      targetRevision = each.value.target_revision
+    })
+  ]
+}
--- stack/mariadb-operator/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "mariadb_password" {
-    value = random_password.mariadb_password.result
-    sensitive = true
-}
