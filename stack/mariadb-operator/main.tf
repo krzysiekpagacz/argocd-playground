3328155cb1809d2174216313b6df8cc88e1d3296
krzpag <Krzysztof-Tomasz.Pagacz@t-systems.com>
2026-01-02 15:30:45 +0100

    add monitoring

--- stack/mariadb-operator/main.tf
+++ stack/mariadb-operator/main.tf
@@ -22,10 +22,11 @@ resource "helm_release" "mariadb_operator" {
       targetRevision = each.value.target_revision
       namespace      = each.value.namespace
       cluster = {
-        clusterName   = each.key
-        replicas      = try(each.value.cluster.replicas, 3)
-        galeraEnabled = try(each.value.cluster.galera.enabled, false)
-        tlsEnabled    = try(each.value.tls.enabled, false) #ToDo(kp): make default to true before merge
+        clusterName       = each.key
+        replicas          = try(each.value.cluster.replicas, 3)
+        monitoringEnabled = try(each.value.cluster.monitoring.enabled, false)
+        galeraEnabled     = try(each.value.cluster.galera.enabled, false)
+        tlsEnabled        = try(each.value.tls.enabled, false) #ToDo(kp): make default to true before merge
       }
     })
   ]
